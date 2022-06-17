bdf206775ac3e46fa1fa54143d7ab9e1d803ead2
ahlamchakrane <ahlamchakrane@hotmail.fr>
2022-06-17 12:54:27 +0100

    Revert "design"
    This reverts commit ac88c5093c4891eb101ab6a2ec6ce1390c5562bb.

--- ProjetMobile/app/src/main/java/com/example/projetmobile/HistoryActivity.java
+++ ProjetMobile/app/src/main/java/com/example/projetmobile/HistoryActivity.java
@@ -1,131 +1,14 @@
 package com.example.projetmobile;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.DatePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+public class HistoryActivity extends AppCompatActivity {
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Map;
-
-public class HistoryActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
-    private RecyclerView recyclerView;
-
-    private TodayItemsAdapter todayItemsAdapter;
-    private List<Data> list;
-
-    private FirebaseAuth firebaseAuth;
-    private String onlineUserId = "";
-    private DatabaseReference expensesRef, personalRef;
-
-    private Button search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-        search = findViewById(R.id.search);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        onlineUserId = firebaseAuth.getCurrentUser().getUid();
-
-        recyclerView = findViewById(R.id.recyclerViewHistory);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setReverseLayout(true);
-        layoutManager.setStackFromEnd(true);
-        recyclerView.setLayoutManager(layoutManager);
-
-        list = new ArrayList<>();
-        todayItemsAdapter = new TodayItemsAdapter(HistoryActivity.this, list);
-        recyclerView.setAdapter(todayItemsAdapter);
-
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDatePickerDialog();
-            }
-        });
     }
-    private void showDatePickerDialog(){
-        DatePickerDialog datePickerDialog = new DatePickerDialog(
-                this,
-                this,
-                Calendar.getInstance().get(Calendar.YEAR),
-                Calendar.getInstance().get(Calendar.MONTH),
-                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-        );
-        datePickerDialog.show();
-    }
-
-    @Override
-    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-        int months = month+1;
-        String d, m;
-        if(String.valueOf(dayOfMonth).length()==1){
-            d = "0"+dayOfMonth;
-        }else {
-            d=""+dayOfMonth;
-        }
-        if(String.valueOf(months).length()==1){
-            m= "0"+months;
-        } else {
-            m=""+months;
-        }
-        String date = d+"-"+m+"-"+year;
-        Log.e("date",date);
-      //  Toast.makeText(this, date, Toast.LENGTH_SHORT).show();
-
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
-        Query query = reference.orderByChild("date").equalTo(date);
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                list.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Data data = snapshot.getValue(Data.class);
-                    list.add(data);
-                }
-                todayItemsAdapter.notifyDataSetChanged();
-                recyclerView.setVisibility(View.VISIBLE);
-
-                int totalAmount = 0;
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    Map<String, Object> map = (Map<String, Object>) ds.getValue();
-                    Object total = map.get("amount");
-                    int pTotal = Integer.parseInt(String.valueOf(total));
-                    totalAmount += pTotal;
-
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(HistoryActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-    public void logout(View v){
-        Intent intent = new Intent(HistoryActivity.this, AccountActivity.class);
-        startActivity(intent);
-    }
-
 }
--- ProjetMobile/app/src/main/java/com/example/projetmobile/RegistrationActivity.java
+++ ProjetMobile/app/src/main/java/com/example/projetmobile/RegistrationActivity.java
@@ -1,88 +1,14 @@
 package com.example.projetmobile;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
-    private EditText email, password,confirmPassword;
-    private Button registerBtn;
-    private TextView registerQn;
-    private FirebaseAuth firebaseAuth;
-    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        confirmPassword = findViewById(R.id.confirmPassword);
-        registerBtn = findViewById(R.id.registerBtn);
-        registerQn = findViewById(R.id.registerQn);
-        //firebase
-        firebaseAuth = FirebaseAuth.getInstance();
-        progressDialog = new ProgressDialog(this);
-
-        registerQn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String emailString = email.getText().toString();
-                String passwordString = password.getText().toString();
-                String confirmPasswordString = confirmPassword.getText().toString();
-                if(TextUtils.isEmpty(emailString)){
-                    email.setError("Email is required");
-                }
-                if(TextUtils.isEmpty(passwordString)){
-                    password.setError("Email is required");
-                }
-                if(TextUtils.isEmpty(confirmPasswordString)){
-                    confirmPassword.setError("Email is required");
-                }
-                if(! confirmPasswordString.equals(passwordString)){
-                    Toast.makeText(RegistrationActivity.this, "Password does not match", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    progressDialog.setMessage("Registration in progress");
-                    progressDialog.setCanceledOnTouchOutside(false);
-                    progressDialog.show();
-                    firebaseAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()){
-                                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                                progressDialog.dismiss();
-                            } else {
-                                Toast.makeText(RegistrationActivity.this, "An error has occurred", Toast.LENGTH_SHORT).show();
-                                progressDialog.dismiss();
-                            }
-                        }
-                    });
-                }
-            }
-        });
     }
 }
